2853629a8ee3d5abc597a6b771cb1f94f7d68c0d
hplusplusplusplus <hplusplusplusplus@cf185c00-02bf-11df-bca2-d32778fd5087>
2010-04-05 22:43:54 +0000

    Sorry forgot to comment the last two commits: 156 was adding topictimecounts 157 was adding stats functionality
    NOW i added the help screen stuff.
    
    Jonathan

--- MindBlasterLatest/Classes/HelpScreenController.m
+++ MindBlasterLatest/Classes/HelpScreenController.m
@@ -11,11 +11,13 @@
 
 
 @implementation HelpScreenController
-@synthesize help;
+@synthesize text;
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
+	
+	[text setEditable:NO];
 	
 	NSLog(@"parent: %@", [self.navigationController title]);
 	
@@ -113,42 +115,117 @@
 
 // change label to respond to parent
 -(void) respondToRoot {
-	[help setText: @"replace with script for root"];
+ 
+	[text setText: @"INTRO SCREEN HELP\n\n"
+	 
+					 "New Game -	Create a new account and start a new game\n\n"
+	 
+					 "Load Game -	Load existing account and game"];
 }
 
 // change label to respond to parent
 -(void) respondToTopic {
-	[help setText: @"replace with script for topic"];
+	[text setText: @"TOPIC SCREEN HELP\n\n"
+	 
+					"Touch the symbol of the topic you want to practice here:\n\n"
+	 
+					"+		- addition\n"
+					"-		- subtrction\n"
+					"x		- multiplication\n"
+					"/		- division\n\n"
+	 
+					"OK - Play the topic you chose\n"
+					"BACK - Go back and choose another name/email"];
 }
 
 // change label to respond to parent
 -(void) respondToCharacter {
-	[help setText: @"replace with script for character"];
+	[text setText: @"CHARACTER SCREEN HELP\n\n"
+	 
+					"Touch the character you wish to be\n\n"
+	 
+					"BACK - Go back to the intro screen"];
 }
 
 // change label to respond to parent
 -(void) respondToDifficulty {
-	[help setText: @"replace with script for difficulty"];
+	[text setText: @"DIFFICULTY SCREEN HELP\n\n"
+					
+					"Touch the button of the difficulty you will play\n\n"
+					
+					"EASIEST	- the questions only have numbers between 0 and 10\n\n"
+	 
+					"EASY		- the questions only have numbers between 0 and 20\n\n"
+	 
+					"HARD		- the questions only have numbers between 0 and 30\n\n"
+	 
+					"HARDEST	- the questions only have numbers between 0 and 40\n\n"
+	 
+					"OK - Play the difficulty you chose\n"
+					"BACK - Go back and change you topic you chose"];
 }
 
 // change label to respond to parent
 -(void) respondToUsername {
-	[help setText: @"replace with script for username"];
+	[text setText: @"USERNAME SCREEN HELP\n\n"
+					
+					"Touch the empty boxes to type in your name and email address\n\n"
+	 
+					"OK - continue and use the name and email you typed in\n\n"
+	 
+					"BACK - Go back and choose another profile picture"];
 }
 
 // change label to respond to parent
 -(void) respondToNetwork {
-	[help setText: @"replace with script for network"];
+	[text setText: @"LOADGAME SCREEN HELP\n\n"
+	 
+	 			"UPLOAD		- Save your progress to the website (to keep it safe, or to download it to your friends iPhone)\n\n"
+	 
+	 "DOWNLOAD	- Get your profile from the website (NOTE: the current profile on this iPhone will be lost)\n\n"
+	 
+	 "BACK - Go back to the intro screen"];
 }
 
 // change label to respond to parent
 -(void) respondToGameScreen {
-	[help setText: @"replace with script for gamescreen"];
+	[text setText: @"GAMESCREEN HELP\n\n"
+	 
+					"----- SCREEN LAYOUT -----\n\n"
+	 
+					"The top of the screen (from left to right) shows your CHARACTER PICTURE, CURRENT DIFFICULTY, PAUSE BUTTON, LIVES REMAINING and YOUR SCORE.  Below your LIVES"
+					"REMAINING is your SHIELD BAR which shows how much shields you have on your current life\n\n"
+	 
+					"In the center of your screen is your SHIP\n\n"
+	 
+					"The left side is the ROTATION WHEEL, which rotates your ship\n\n"
+	 
+					"The right side is the FIRE BUTTON, which fires your ships lasers\n\n"
+	 
+					"The bottom center is the QUESTION you have to answer and under that is how much time you've spent on the current topic.\n\n"
+	 
+					"The top right is the help button, which you obviously know or you wouldn't be reading this\n\n"
+	
+					"----- HOW TO PLAY -----\n\n"
+	 
+					"You should shoot the asteroid that has the answer to the QUESTION shown in the bottom middle.\n"
+					"If you get hit by asteroids, you will lose shield power.  When you have no shields getting hit by an asteroid will make you lose the game.\n"
+					"If you shoot blank asteroids you get XX points.\n"
+					"If you shoot the correct asteroid you get XX points\n"
+					"If you shoot the wrong asteroid you lose XX points and if you have less than 0 points you lose the game\n\n"
+					
+					"For every XX points you will go to the next difficulty.\n"
+					"For every XX points you will go to the next topic.\n"
+					];
 }
 
 // change label to respond to parent
 -(void) respondToGameOver {
-	[help setText: @"replace with script for gameover"];
+	[text setText: @"GAMEOVER SCREEN HELP\n\n"
+	
+					"TRY AGAIN		- play the game again from the last topic and difficulty you achieved\n"
+					"CHANGE TOPIC	- go to the topic screen and choose another topic to play\n"
+					"MAIN MENU		- go back to the intro screen"];
 }
 
 
